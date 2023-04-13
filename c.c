a203f7b3e78664c3c019e1fe9dd3267211f426b3
parthar409 <parthar40888@gmail.com>
2023-04-13 17:47:55 +0600

    2nd try

---
+++ c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ int main()
+  {
+    int a;
+    printf("enter tha namber");
+    scanf("%d",&a);
+    if(a<=40)
+    { 
+        printf("you are not select");
+    }
+   else   if (a>=80)
+     {
+        printf("you are not select");
+    }
+
+    else{
+        printf("you  are right person");
+    }
+    return 0;
+
+  } 
