1482b3e7f30a4685b527578d69c8ded544a05c2a
parthar409 <parthar40888@gmail.com>
2023-04-13 15:49:39 +0600

    start

---
+++ indrx.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter tha namber");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("%d is even",a);
+    }
+    else{
+        printf("tha namber is odd",a);
+        
+    }
+    return 0;
+    
+    
+}
+
+
+
+
