a3c1cff3df82322ba656f2e26ba5bcc90136cea7
parthar409 <parthar40888@gmail.com>
2023-04-16 19:36:02 +0600

    minimam namber

---
+++ conditional-operator-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+  {
+    int x,y,result;
+    printf("enter tha x and y number : ");
+    scanf("%d%d",&x,&y);
+    result=(x<=y) ? x:y;
+    printf("min number is %d",result);
+ return 0;
+  } 
