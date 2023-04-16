02ef0e25da1e703b483b1710dfe50f845d9e0bcd
parthar409 <parthar40888@gmail.com>
2023-04-16 19:27:30 +0600

    max namber

---
+++ conditionalc-operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main()
+  {
+int x,y,result;
+printf("enter tha x and y number : ");
+scanf("%d%d",&x, &y);
+result=(x>=y)? x:y;
+printf("max is %d",result);
+
+ return 0;
+  } 
