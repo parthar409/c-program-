3b6f46397bb8f1a68593a60e0d3c53453b46193a
parthar409 <parthar40888@gmail.com>
2023-04-17 13:20:41 +0600

    m

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+  {
+ int a,b;
+ printf("Input tha valu of a : ");
+ scanf("%d",&a);
+ printf("Input tha valu of b : ");
+ scanf("%d",&b);
+ if(a>b){
+    printf("a is greater then b");
+ }
+ else{
+    printf("a is less then or equal to b");
+ }
+ return 0;
+  } 
